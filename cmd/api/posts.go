95d7bdce751e44d82673d8b8fd33dfa65ae67fa4
ParintornYaimai <parintorn-y@rmutp.ac.th>
2025-11-19 21:22:39 +0700

    feat: add post

---
+++ cmd/api/posts.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/ParintornYaimai/socialmedia-go/internal/store"
+	"github.com/go-chi/chi/v5"
+)
+
+type CreatePostPlayload struct {
+	Title   string   `json:"title" validate:"required, max=100"`
+	Content string   `json:"content" validate:"required, max=1000"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload CreatePostPlayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if payload.Content == "" {
+		app.badRequestResponse(w, r, fmt.Errorf("content in required"))
+		return
+	}
+
+	post := &store.Post{
+		Title:   payload.Content,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		UserID:  1,
+	}
+	ctx := r.Context()
+
+	if err := app.store.Posts.Create(ctx, post); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+
+	idParam := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+
+	if err != nil {
+		app.internalServerError(w, r, err)
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetById(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound): //errors.Is ใช้ตรวจสอบว่า error ตรงกับ error ที่เราสนใจหรือไม่
+			app.notFoundResponse(w, r, err)
+		default:
+			app.internalServerError(w, r, err)
+		}
+		return
+	}
+
+	comments, err := app.store.Comments.GetByPostID(ctx, id)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	post.Comments = comments
+
+	if err := writeJSON(w, http.StatusInternalServerError, post); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
